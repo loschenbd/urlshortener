f2422f0a45a3c190a3c8cc58b78b879ed2a2b9f3
Benjamin Loschen <loschenbd@gmail.com>
2020-09-01 09:38:30 -0500

    Built initial Schemas and Models. Hooked up json creation on input post.

--- server.js
+++ server.js
@@ -3,15 +3,8 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 require('dotenv').config();
-mongoose.connect("mongodb+srv://"+ process.env.MONGO_USER +":"+ process.env.MONGO_PASS +"@cluster0.nlxxb.mongodb.net/" +process.env.MONGO_DB
-, {useNewUrlParser: true,
-  useUnifiedTopology: true});
-mongoose.connection.on('error', (err) => {
-  console.error(`Mongoose connection error: ${err}`);
-  process.exit(1);
-});
+
 var cors = require('cors');
 require('dotenv').config();
 
@@ -23,11 +16,19 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.DB_URI);
+mongoose.connect("mongodb+srv://"+ process.env.MONGO_USER +":"+ process.env.MONGO_PASS +"@cluster0.nlxxb.mongodb.net/" +process.env.MONGO_DB
+  , {useNewUrlParser: true,
+    useUnifiedTopology: true});
+mongoose.connection.on('error', (err) => {
+  console.error(`Mongoose connection error: ${err}`);
+  process.exit(1);
+});
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+var bodyParser = require('body-parser');
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -35,12 +36,32 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
   
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// Create URL Schema
+let urlSchema = new mongoose.Schema({
+  stored_url: {type: String, required: true},
+  short: Number
+});
+
+// Create URL Model
+let Url = mongoose.model('Url', urlSchema);
+let resObj = {};
+
+app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }) , (req, res) => {
+  let inputLink = req.body['url'];
+  resObj['stored_url'] = inputLink;
+
+  res.json(resObj);
+});
+
+// Listen
 let listener = app.listen(process.env.PORT, () => {
   console.log("Node.js is listening on http://localhost:" + listener.address().port);
 });
+
